0aa5df555f935d825eeae920e28e8d37f2e724e3
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2023-12-29 22:55:35 +1300

    Create Hosting

---
+++ LXGaming.Configuration.Hosting/ConfigurationService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LXGaming.Configuration.Hosting;
+
+internal sealed class ConfigurationService : IHostedService {
+
+    public ConfigurationService(IServiceProvider serviceProvider) {
+        // Ensure that the IConfiguration is disposed with the IServiceProvider.
+        _ = serviceProvider.GetRequiredService<IConfiguration>();
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken) {
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) {
+        return Task.CompletedTask;
+    }
+}
---
+++ LXGaming.Configuration.Hosting/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LXGaming.Configuration.Hosting;
+
+public static class Extensions {
+
+    public static IHostBuilder UseConfiguration(this IHostBuilder builder, IConfiguration configuration) {
+        return builder.ConfigureServices((_, collection) => {
+            collection.AddConfiguration(configuration);
+        });
+    }
+
+    public static IServiceCollection AddConfiguration(this IServiceCollection services, IConfiguration configuration) {
+        return services
+            .AddSingleton<IConfiguration>(_ => configuration)
+            .AddHostedService<ConfigurationService>();
+    }
+}
