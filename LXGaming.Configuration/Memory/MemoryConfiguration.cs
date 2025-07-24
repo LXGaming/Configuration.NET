abfa7231150dc80c31d0ed33becbbc0269d8deae
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-07-24 16:47:58 +1200

    Add memory configuration

---
+++ LXGaming.Configuration/Memory/MemoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using LXGaming.Configuration.Generic;
+
+namespace LXGaming.Configuration.Memory;
+
+public class MemoryConfiguration<T> : IConfiguration<T> where T : new() {
+
+    /// <inheritdoc />
+    public T? Value { get; } = new();
+
+    /// <inheritdoc />
+    public Task LoadAsync(CancellationToken cancellationToken = default) {
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public Task SaveAsync(CancellationToken cancellationToken = default) {
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public void Dispose() {
+        GC.SuppressFinalize(this);
+    }
+}
