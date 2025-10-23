a7bfc5fab671db7f45daae53ed634d5a39c782ce
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-10-23 20:07:21 +1300

    Add Tests

---
+++ LXGaming.Configuration.Tests/File/Json/JsonFileConfigurationTest.cs
@@ -0,0 +1,81 @@
+﻿using LXGaming.Configuration.File.Json;
+using LXGaming.Configuration.Tests.Models;
+using NUnit.Framework;
+
+namespace LXGaming.Configuration.Tests.File.Json;
+
+public class JsonFileConfigurationTest {
+
+    private const string Path = "config.json";
+    private string _data;
+
+    [SetUp]
+    public async Task SetupAsync() {
+        // Generate data
+        _data = CreateData();
+        // Write configuration contents
+        await System.IO.File.WriteAllTextAsync(Path, CreateContents(_data));
+    }
+
+    [Test]
+    public async Task TestLoad() {
+        // Load configuration
+        using var configuration = await JsonFileConfiguration<Config>.LoadAsync();
+        // Test generated data
+        Assert.That(configuration.Value?.Data, Is.EqualTo(_data));
+    }
+
+    [Test]
+    public async Task TestSave() {
+        // Load configuration
+        using var configuration = await JsonFileConfiguration<Config>.LoadAsync();
+        // Test generated data
+        Assert.That(configuration.Value?.Data, Is.EqualTo(_data));
+
+        // Regenerate data
+        configuration.Value.Data = CreateData();
+        // Save configuration
+        await configuration.SaveAsync();
+
+        // Read configuration contents
+        var contents = await System.IO.File.ReadAllTextAsync(Path);
+        // Test configuration contents
+        Assert.That(contents, Is.EqualTo(CreateContents(configuration.Value.Data)));
+    }
+
+    [Test]
+    public void TestCancelledLoad() {
+        // Attempt to load configuration
+        using var configuration = new JsonFileConfiguration<Config>();
+        var cancellationToken = new CancellationToken(true);
+        Assert.ThrowsAsync<TaskCanceledException>(async () => await configuration.LoadAsync(cancellationToken));
+        Assert.That(configuration.Value, Is.Null);
+    }
+
+    [Test]
+    public async Task TestCancelledSave() {
+        // Load configuration
+        using var configuration = await JsonFileConfiguration<Config>.LoadAsync();
+        // Test generated data
+        Assert.That(configuration.Value?.Data, Is.EqualTo(_data));
+
+        // Regenerate data
+        configuration.Value.Data = CreateData();
+        // Attempt to save configuration
+        var cancellationToken = new CancellationToken(true);
+        Assert.ThrowsAsync<TaskCanceledException>(async () => await configuration.SaveAsync(cancellationToken));
+
+        // Read configuration contents
+        var contents = await System.IO.File.ReadAllTextAsync(Path, CancellationToken.None);
+        // Test configuration contents
+        Assert.That(contents, Is.EqualTo(CreateContents(_data)));
+    }
+
+    private static string CreateContents(string data) {
+        return $"{{\"data\":\"{data}\"}}";
+    }
+
+    private static string CreateData() {
+        return Guid.NewGuid().ToString();
+    }
+}
---
+++ LXGaming.Configuration.Tests/Models/Config.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+using YamlDotNet.Serialization;
+
+namespace LXGaming.Configuration.Tests.Models;
+
+public class Config {
+
+    [JsonPropertyName("data")]
+    [YamlMember(Alias = "data")]
+    public string Data { get; set; } = "";
+}
